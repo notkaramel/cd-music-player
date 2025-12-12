cd1adcb09e29c8c83b6544ec867a98b5355aba4f
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-12-11 21:16:37 -0500

    Player update

--- player/main.go
+++ player/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("cdda2wav", "-v", "255", "-B")
-	output, err := cmd.CombinedOutput()
+	// 1. Define the command.
+	// We use exec.Command to prepare the execution of the neofetch program.
+
+	cmd := exec.Command("echo", "Hello, World!")
+
+	// 2. Direct the output (Stdout) and error stream (Stderr).
+	// By setting Stdout to os.Stdout, the output from neofetch will be
+	// printed directly to the standard output of the Go program (your terminal).
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// 3. Execute the command.
+	// cmd.Run() starts the specified command and waits for it to complete.
+	err := cmd.Run()
+
 	if err != nil {
-		log.Fatalf("Error: %v\n", err)
+		log.Fatalf("Command failed to run: %v", err)
 	}
-	fmt.Println(string(output))
 }
